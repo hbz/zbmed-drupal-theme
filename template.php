34e0d8890ecfedb6399e35b48a0ee27333f740cf
Jan on Test <schnasse@gmx.de>
2014-12-10 08:54:18 +0100

    link via https to google fonts and include font awesome

--- template.php
+++ template.php
@@ -15,14 +15,14 @@
   */
 
   // Droid font family: a complete family with serif and sans
-  drupal_add_css('http://fonts.googleapis.com/css?family=Droid+Serif|Droid+Sans:400,700&subset=latin,latin-ext', array('type' => 'external'));
+    drupal_add_css('https://fonts.googleapis.com/css?family=Droid+Serif|Droid+Sans:400,700&subset=latin,latin-ext', array('type' => 'external'));
   // Signika font: only serif 
-  drupal_add_css('http://fonts.googleapis.com/css?family=Signika:400,600,700,300&subset=latin,latin-ext', array('type' => 'external'));
+   drupal_add_css('https://fonts.googleapis.com/css?family=Signika:400,600,700,300&subset=latin,latin-ext', array('type' => 'external'));
   // PT font family: a complete family with serif and sans
-  //drupal_add_css('http://fonts.googleapis.com/css?family=PT+Serif|PT+Sans|PT+Sans+Narrow|PT+Sans+Caption&subset=latin,latin-ext', array('type' => 'external'));
+  drupal_add_css('https://fonts.googleapis.com/css?family=PT+Serif|PT+Sans|PT+Sans+Narrow|PT+Sans+Caption&subset=latin,latin-ext', array('type' => 'external'));
 
   // Add Font-Awesome
-  drupal_add_css('http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css');
+  drupal_add_css('css/font-awesome.min.css');
 
   }
 
