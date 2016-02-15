557ef8ef8f8e2b2f84f5a6b0a316303092d4301b
Andres Quast <quast@hbz-nrw.de>
2016-02-15 17:57:55 +0100

    starting workaround for theme

---
+++ theme.js
@@ -0,0 +1,24 @@
+(function($) {
+
+
+
+  Drupal.behaviors.edoweb_drupal_theme_entity_minimize = {
+    attach: function (context, settings) {
+
+      $('.field-name-field-edoweb-parallel .field-label').text('HT-Nummer');
+
+      
+    }
+  };
+
+  //Drupal.behaviors.edoweb_drupal_theme_datepicker = {
+  //  attach: function (context, settings) {
+  //    // datepicker
+  //    $('#edit-field-edoweb-issued-und-0-value', context).datepicker({
+  //      changeMonth:true,
+  //      changeYear:true,
+  //      dateFormat:"dd.mm.yy" 
+  //    });
+  //  }
+  //};
+})(jQuery);
