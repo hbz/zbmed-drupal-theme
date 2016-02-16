cfa4fed98e98124a6a38f6e2087a4afc5d112e73
Andres Quast <quast@hbz-nrw.de>
2016-02-16 06:12:26 +0100

    proceed

--- theme.js
+++ theme.js
@@ -5,10 +5,17 @@
   Drupal.behaviors.edoweb_drupal_theme_entity_minimize = {
     attach: function (context, settings) {
 
+      // find the correct field
       $('.field-name-field-edoweb-parallel .field-label').text('HT-Nummer');
+      // get HT-Number
       var htnr = $('.field-name-field-edoweb-parallel .resolved').attr('data-curie');
-      htnr.replace("lr:","");
-      $('.field-name-field-edoweb-parallel').append('<div class="field-items">'+ htnr + '</div>');
+      htnr.replace("Ir:","");
+      // replace old html-code with code to display HT-Number only
+      $('.field-name-field-edoweb-parallel table').remove();
+      $('.field-name-field-edoweb-parallel').append('<div class="field-items">'
+      + '<table class=""><tbody><td>'
+      + htnr + 
+      '</td></tbody></table></div>');
 
       
     }
