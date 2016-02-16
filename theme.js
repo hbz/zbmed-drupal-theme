ce8e4de3d3567d665011b096a46b8c5ad2846b84
Andres Quast <quast@hbz-nrw.de>
2016-02-16 21:56:05 +0100

    replace child if child is file

--- theme.js
+++ theme.js
@@ -30,4 +30,31 @@
     }
   };
 
+  Drupal.behaviors.edoweb_drupal_theme_child = {
+    attach: function (context, settings) {
+
+    var fieldLabel = 'Datei';
+
+    $('.field-name-field-edoweb-struct-child', context).ajaxComplete(function() {
+      //$(this).find('.field-label').text('HT-Nummer');
+      var isFile = $(this).find('.download');
+      if( isFile ) {
+        $(this).find('.item-label').text(fieldLabel);
+      }
+      /*
+      $(this).find('table').remove();
+      $('.field-name-field-edoweb-parallel').once().append( '<div class="field-items"><div class="field-item even">'
+         + '<a target="_blank" href="http://193.30.112.134/F/?func=find-c&ccl_term=IDN%3D' + htnr + '"'
+         + ' data-target-bundle="monograph" data-curie="lr:' + htnr + '" resource="http://lobid.org/resource/' + htnr + '" class="resolved">'
+         + htnr +'</a>'
+         + '</div></div>');
+      $('.field-name-field-edoweb-doi').after($(this));
+ */
+      });
+
+
+    }
+  };
+
+
 })(jQuery);
