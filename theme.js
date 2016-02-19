9ca3230493a11cac04830c5e77bdb6f392a99edd
aquast <quast@hbz-nrw.de>
2016-02-19 17:59:39 +0100

    reduced jquery code by replacing it with css

--- theme.js
+++ theme.js
@@ -1,7 +1,5 @@
 (function($) {
 
-
-
   Drupal.behaviors.edoweb_drupal_theme_entity_minimize = {
     attach: function (context, settings) {
 
@@ -12,17 +10,20 @@
       htnr = htattr.replace('lr:','');
     };
 
-
+    // replace Label "Titelkopie von" with fieldLabel
     $('.field-name-field-edoweb-parallel .field-label', context).text(fieldLabel);
+
     $('.field-name-field-edoweb-parallel', context).ajaxComplete(function() {
       //$(this).find('.field-label').text('HT-Nummer');
-      $(this).find('table').remove();
-      $('.field-name-field-edoweb-parallel').once().append( '<div class="field-items"><div class="field-item even">'
+      //alert($(this).find('table').html());   //.hide();
+
+      $(this, context).once().append( '<div class="field-items"><div class="field-item even">'
          + '<a target="_blank" href="http://193.30.112.134/F/?func=find-c&ccl_term=IDN%3D' + htnr + '"'
          + ' data-target-bundle="monograph" data-curie="lr:' + htnr + '" resource="http://lobid.org/resource/' + htnr + '" class="resolved">'
          + htnr +'</a>'
          + '</div></div>');
-      $('.field-name-field-edoweb-doi').after($(this));
+      $('.field-name-field-edoweb-medium').after($(this));
+      //$(this, context).find('table').hide();
 
       });
 
@@ -31,3 +32,4 @@
   };
 
 })(jQuery);
+
