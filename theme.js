87ba13ef1e8d7f45304b385d1461947685492f67
aquast <quast@hbz-nrw.de>
2016-03-08 14:47:10 +0100

    reworked code for displaying thumb at parent: display parent only if child is file

--- theme.js
+++ theme.js
@@ -29,39 +29,49 @@
   Drupal.behaviors.edoweb_drupal_theme_child = {
     attach: function (context, settings) {
 
-    var fieldLabel = 'Datei';
+    var fieldLabelParent = 'Dateiliste:';
+    var fieldLabel = 'Download:';
     var thumbyUrl = 'https://api.ellinet-dev.hbz-nrw.de/tools/thumby?url=';
     var thumbSize = '&size=150';
     var serverUrl = 'https://ellinet-dev.hbz-nrw.de'
 
     $('.field-name-field-edoweb-struct-child', context).ajaxComplete(function() {
-      var dataLink = $('a[data-target-bundle*="file"]');
-      if( dataLink.html() ) {
 
-        var ref = $(this).attr('href');
-        $(this).find('.field-label:first').text('Dateiliste:');
-        var firstLink = $('.download').attr('href');
-        var pictureField = '<div class="field field-name-field-edoweb-preview"><div class="field-label"></div>'
-          + '<div class="field-items">'
-          // + '<div class="field-item" style="display:none" property="dc:format">image/jpeg</div>' 
-          + '<div class="field-item thumb" property="regal:hasPart">'
-          + '<a href="' + serverUrl + firstLink + '" target="_blank">'
-          + '<img src="'
-          + thumbyUrl
-          + serverUrl
-          + firstLink
-          + thumbSize
-          + '" /></a></div></div></div>';
-        }
+      $(this).find('.edoweb[data-entity-bundle="file"').once(function() {
 
-      $('.field-name-field-edoweb-title').once().after($(pictureField));
-      $('.field-name-field-edoweb-preview').after($(this));
+       $('.field-name-field-edoweb-struct-child .field-label').text(fieldLabel);
+       $('.field-name-field-edoweb-struct-child').find('.field-label:first').text(fieldLabelParent);
+         var dataLink = $(this).find('h1 a').attr('href') + '/data';
+         var mimetype = $(this).find('.download').attr('title').replace(/Download /, '').toLowerCase().replace(/-/, '/');
+         var pictureField = '<div class="field field-name-field-edoweb-preview">'
+           + '<div class="field-label"></div>'
+           + '<div class="field-item thumb" >'
+           + '<a href="' + serverUrl + dataLink + '" target="_blank">'
+           + '<img src="'
+           + thumbyUrl
+           + serverUrl
+           + dataLink
+           + thumbSize
+           + '" /></a>'
+           + '</div>'
+           + '<div class="field field-name-field-edoweb-filetype" >'
+           + '<div class="field-items">'
+           + '<div class="field-item" property="dc:format" style="display: none;">' + mimetype + '</div>'
+           + '</div>'
+           + '</div>'
+           + '</div>';
+
+         $('.field-name-field-edoweb-title').once().after($(pictureField));
+
+        });
+
+       $('.field-name-field-edoweb-preview').after($(this));
+
       });
 
 
     } 
   };
-
 
 })(jQuery);
 
