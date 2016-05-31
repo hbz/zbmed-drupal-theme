bf2cf341551b6617646ce73ea94d780214bd3151
Jan Schnasse <jan@schnasse.eu>
2016-05-31 16:08:02 +0200

    remove field overrides in theme.js - @aquast not sure what remaining code under theme.js#edoweb_drupal_theme_entity_minimize is for. Thumbnailer will not be displayed correctly if change one of the following lines https://github.com/edoweb/zbmed-drupal-theme/blob/master/theme.js#L4

--- theme.js
+++ theme.js
@@ -2,27 +2,16 @@
 
   Drupal.behaviors.edoweb_drupal_theme_entity_minimize = {
     attach: function (context, settings) {
-
-    var fieldLabel = 'Verbundkatalog-ID:';
+    	
     var htattr = $('.field-name-field-edoweb-parallel .resolved', context).attr('data-curie');
     var htnr = '';
     if( htattr ){
       htnr = htattr.replace('lr:','');
     };
-
-    // replace Label "Titelkopie von" with fieldLabel
-    $('.field-name-field-edoweb-parallel .field-label', context).text(fieldLabel);
-
     $('.field-name-field-edoweb-parallel', context).ajaxComplete(function() {
-      $('.field-name-field-edoweb-parallel').once().append( '<div class="field-items"><div class="field-item even">'
-         + '<a target="_blank" href="http://193.30.112.134/F/?func=find-c&ccl_term=IDN%3D' + htnr + '"'
-         + ' data-target-bundle="monograph" data-curie="lr:' + htnr + '" resource="http://lobid.org/resource/' + htnr + '" class="resolved">'
-         + htnr +'</a>'
-         + '</div></div>');
+      $('.field-name-field-edoweb-parallel').once().append( '<div class="field-items"></div>'); 
       $('.field-name-field-edoweb-medium').after($(this));
       });
-
-
     }
   };
 
@@ -38,7 +27,7 @@
    
     $('.field-name-field-edoweb-struct-child', context).ajaxComplete(function() {
 
-     if($('.active :contains("View")').html() == null && $('.active :contains("Ansicht")').html() == null){
+     if($('.active :contains("Edit")').html() != null || $('.active :contains("Bearbeiten")').html() != null){
     	    	return;
      }
     	    
