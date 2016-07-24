cc4ead794886695646d3b1bd0727e4b95e095268
Andres Quast <quast@hbz-nrw.de>
2016-07-24 16:22:54 +0200

    hide isbn10

--- js/isbnformat.js
+++ js/isbnformat.js
@@ -21,12 +21,13 @@
           var url = serviceurl + service + isbn + callback;
     
           $.getJSON(url, function(json) {
-             $('.field-name-field-edoweb-isbn10 .field-item').html(json.result.isbn10formatted);
+             $('.field-name-field-edoweb-isbn10 .field-item').html(json.result.isbn13formatted);
             
-             $('.field-name-field-edoweb-isbn10').hide()
+             $('.field-name-field-edoweb-isbn10')
                 .after('<div class="field-name-field-edoweb-isbn13"></div>')
                 .append('<div class="field-label">ISBN-13:</div><div class="field-item">' + json.result.isbn13formatted + '</div>');
-       
+             
+             $('.field-name-field-edoweb-isbn10').hide();
              });
           };
   
