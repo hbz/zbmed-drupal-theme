8de7183c97f8a3bcf31b7a900102f6c55719b50b
Andres Quast <quast@hbz-nrw.de>
2016-07-25 13:49:18 +0200

    added attribute placeholder

--- js/searchbutton.js
+++ js/searchbutton.js
@@ -4,6 +4,7 @@
     Drupal.behaviors.edoweb_drupal_theme_searchbutton = {
     attach: function (context, settings) {
 
+      $('#edit-query-0-term', context).attr('placeholder', 'Repository-Suche');
       replaceWithButton($('#edit-query-0-submit'), 'large-batch-icons batch-icon-search', context);
 
       function replaceWithButton(target, iconCss) {
