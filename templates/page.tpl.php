fc485c54dc3b3a3d7304e406bcbf3e63e26826d2
Jan Schnasse <schnasse@gmx.de>
2017-07-20 10:23:28 +0200

    render page title from variable pageTitle if possible - pageTitle is provided by the regal-drupal module in edoweb/edoweb.module#edoweb_preprocess_page() - the value of pageTitle is pure html comming from regal-api

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -188,12 +188,10 @@
       <?php if ($page['highlighted']): ?><div id="highlighted"><?php print render($page['highlighted']); ?></div><?php endif; ?>
       <a id="main-content"></a>
       <?php print render($title_prefix); ?>
-      <?php if ($title): ?>
-        <h1 class="title" id="page-title">
-          <?php print $title; ?>
-        </h1>
+      <?php if (isset($pageTitle)): ?>
+          <?php print $pageTitle; ?>
       <?php endif; ?>
-      <?php print render($title_suffix); ?>
+     
       <?php if ($tabs): ?>
         <div class="tabs">
           <?php print render($tabs); ?>
