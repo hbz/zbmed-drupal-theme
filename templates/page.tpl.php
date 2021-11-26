b09ef69eedfd76f5b3bdf66322da3f531cf259ce
Peter Reimer <reimer@hbz-nrw.de>
2021-11-26 17:00:29 +0100

    FRL-559, Anpassungen an der Startseite

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -91,8 +91,8 @@
 
     <?php $mynewurl='http://www.publisso.de';
     	   if ($logo): ?>
-      	      <a href="<?php print $mynewurl ?>" title="<?php print t('Home'); ?>" rel="home" id="logo">
-              	 <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
+      	      <a href="<?php print $mynewurl ?>" title="zur Publisso Startseite" rel="home" id="logo">
+              	 <img src="<?php print $logo; ?>" alt="Publisso Logo" />
       	      </a>
     <?php endif; ?>
 
@@ -103,12 +103,14 @@
           <?php if ($title): ?>
             <div id="site-name"<?php if ($hide_site_name) { print ' class="element-invisible"'; } ?>>
               <strong>
-                <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
+                <a href="https://www.publisso.de/open-access-publizieren/repositorien/fachrepositorium-lebenswissenschaften/" title="zur Startseite des Fachrepositoriums Lebenswissenschaften" / rel="home"><span><?php print $site_name; ?></span></a>
+                <!--<a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>-->
               </strong>
             </div>
           <?php else: /* Use h1 when the content title is empty */ ?>
             <h1 id="site-name"<?php if ($hide_site_name) { print ' class="element-invisible"'; } ?>>
-              <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
+              <a href="https://www.publisso.de/open-access-publizieren/repositorien/fachrepositorium-lebenswissenschaften/" title="zur Startseite des Fachrepositoriums Lebenswissenschaften" rel="home"><span><?php print $site_name; ?></span></a>
+              <!--<a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>-->
             </h1>
           <?php endif; ?>
         <?php endif; ?>
